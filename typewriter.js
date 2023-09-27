7274c6066e0efafc6702dd778b144dc9d69cfae7
Kevin.L <kevinlew27@gmail.com>
2023-09-27 16:21:04 +0800

    Update typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -6,12 +6,15 @@ class TypeWriter {
     this.txt = 'hello'
     this.speed = 50;
     this.el = null
+    this.timeout = null; // The timeout function controlling the speed of text
   }
 
   /* INITIATE */
   type(selector, txt, speed) {
     this.el = document.querySelector(selector);
     this.el.innerHTML = ''; // RESET
+    this.startPos = 0;
+    clearTimeout(this.timeout);
     this.txt = txt;
     this.speed = speed;
     this.run();
@@ -21,7 +24,7 @@ class TypeWriter {
     if (this.startPos < this.txt.length) {
       this.el.innerHTML += this.txt.charAt(this.startPos);
       this.startPos++;
-      setTimeout(this.run.bind(this), this.speed);
+      this.timeout = setTimeout(this.run.bind(this), this.speed);
     }
   }
 }
